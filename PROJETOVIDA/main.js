f4519f1c0a8f51136d273bed30bba37d2de4a714
josueristow <126601767+josueristow@users.noreply.github.com>
2024-03-07 15:04:12 -0300

    Add files via upload

---
+++ PROJETOVIDA/main.js
@@ -0,0 +1,56 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-13T00:00:00");
+const tempoObjetivo2 = new Date("2024-09-09T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-15T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-31T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
+function CalculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    
+    segundos %= 60;
+    minutos %= 60;
+    horas %=24;
+    
+    
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos  + " segundos ";
+    
+    
+}
+
+function atualizaConometro(){
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = CalculaTempo(tempos[i]);
+
+}
+
+}
+
+function começaConometro(){
+    atualizaConometro();
+    setInterval(atualizaConometro,1000);
+}
+começaConometro();
